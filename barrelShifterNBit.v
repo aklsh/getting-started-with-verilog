30c6607b073bd6ee3919854c2c74c8fa40123bde
Akilesh Kannan <akileshkannan@gmail.com>
2019-09-22 19:40:12 +0530

    added barrel shifter

---
+++ barrelShifterNBit.v
@@ -0,0 +1,11 @@
+module barrelShifterNBit #(parameter n = 4)(output reg[n-1:0] out, input[n-1:0] in, input[$clog2(n):0] shift);
+    integer i;
+    always @ (in, shift) begin
+        for(i = 0; i < n; i = i+1) begin
+            if(i + shift < n)
+                out[i+shift] = in[i];
+            else
+                out[i+shift-n] = in[i];
+        end
+    end
+endmodule
